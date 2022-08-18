0827ed98e9fa56a04db970959dfce892c046c62a
Manuel2u <edodoo011@st.ug.edu.gh>
2022-08-18 23:18:48 +0000

    Fixed Bug

--- Basic Calculator/calculator.js
+++ Basic Calculator/calculator.js
@@ -1,29 +1,26 @@
-(function() {
-    let screen = document.querySelector('.screen');
-    let buttons = document.querySelectorAll('.btn');
-    let clear = document.querySelector('.btn-clear');
-    let equalTo = document.querySelector('.btn-equal');
+let screen = document.querySelector('.screen');
+let buttons = document.querySelectorAll('.btn');
+let clear = document.querySelector('.btn-clear');
+let equalTo = document.querySelector('.btn-equal');
 
-    buttons.forEach(function(button) {
-        button.addEventListener('click', function(e) {
-            let value = e.target.dataset.num;
-            screen.value += value;
-        })
-    });
 
-    equalTo.addEventListener('click', function(e) {
-        if (screen.value === "") {
-            screen.value = "";
-        } else {
-            let answer = eval("screen.value");
-            screen.value = answer;
-        }
+buttons.forEach(function (button) {
+    button.addEventListener('click', function (e) {
+    let value = e.target.dataset.num;
+        
+        screen.value += value;
+        
     })
+});
 
-    clear.addEventListener('click', function(e) {
-        screen.value = "";
-    })
+function answer() {
+        let ans = Function("return " + screen.value)();
+        screen.value = ans;
+    }
+;
 
 
+clear.addEventListener('click', function (e) {
+    screen.value = "";
+});
 
-})();
